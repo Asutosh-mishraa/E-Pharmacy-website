5d10b7c1c20cf3a9c5117bcb8ce67c3455de6adc
Asutosh-mishraa <asumishra25@gmail.com>
2023-02-16 12:41:19 +0530

    added the path of static files to the setings

--- pharmacy/pharmacy/settings.py
+++ pharmacy/pharmacy/settings.py
@@ -122,6 +122,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#creats a list to store the path of static files used in the project
+STATICFILES_DIRS=[
+    BASE_DIR / "static"
+]
+
+# Media files 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
