887498e9e4ba259cf8b260ad2d6b4ef6befc2cfc
Asutosh-mishraa <asumishra25@gmail.com>
2023-04-19 15:55:45 +0530

    some updates

--- pharmacy/products/views.py
+++ pharmacy/products/views.py
@@ -14,3 +14,16 @@ def product_detail(request,id):
         'products' : products,
     }
     return render(request,'products/prod_details.html',context)
+def search(request):
+    product_name=request.GET.get("search")
+    search_result=product.objects.filter(name__icontains=product_name)
+
+    context={
+        "search_result": search_result
+    }
+    return render(request,'products/search.html',context)
+
+def cart(request):
+    return render(request,'products/cart.html')
+def checkout(request):
+    return render(request,'products/checkout.html')
