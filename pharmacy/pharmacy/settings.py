93196d24d13556e5e5ed832c864be772bc2e2c1c
Asutosh-mishraa <91376218+Asutosh-mishraa@users.noreply.github.com>
2023-05-03 09:29:04 +0530

    Update settings.py
    changed the mailing system

--- pharmacy/pharmacy/settings.py
+++ pharmacy/pharmacy/settings.py
@@ -141,7 +141,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT  = 587
-EMAIL_HOST_USER = "asumishra25@gmail.com"
-EMAIL_HOST_PASSWORD  = "unyuzrfudraptaht"
+EMAIL_PORT  = 681
+EMAIL_HOST_USER = "mail@gmail.com"
+EMAIL_HOST_PASSWORD  = "xxxxx"
 EMAIL_USE_TLS = True
